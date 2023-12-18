41740698497ddd0779b0b639e67462a5fab6f74d
Jyoti_Parashar2000 <parasharjyoti2000@gmail.com>
2023-12-18 13:06:00 +0530

    store1

--- task8File.js
+++ task8File.js
@@ -1,12 +1,12 @@
 var express = require("express");
 var app = express();
-const cors = require('cors');
-const corsOptions = {
-    origin: 'http://localhost:3000',
-    credentials: true,            //access-control-allow-credentials:true
-    optionSuccessStatus: 200
-}
-app.use(cors(corsOptions));
+// const cors = require('cors');
+// const corsOptions = {
+//     origin: 'http://localhost:3000',
+//     credentials: true,            //access-control-allow-credentials:true
+//     optionSuccessStatus: 200
+// }
+// app.use(cors(corsOptions));
 app.use(express.json());
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
